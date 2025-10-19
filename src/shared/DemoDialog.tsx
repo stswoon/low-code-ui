90894a0656469cf547157edab2c1caaa41de280b
stswoon <xx@xx.xx>
2025-10-19 20:59:58 +0400

    video

--- src/shared/DemoDialog.tsx
+++ src/shared/DemoDialog.tsx
@@ -1,4 +1,4 @@
-import {Box, Dialog, DialogContent, DialogTitle, Link, Stack} from '@mui/material';
+import {Dialog, DialogContent, DialogTitle, Link, Stack} from '@mui/material';
 import {type FC, memo, useState} from 'react';
 
 export const DemoDialog: FC = memo(() => {
@@ -12,7 +12,7 @@ export const DemoDialog: FC = memo(() => {
                     <Link href="https://github.com/stswoon/low-code-ui/blob/main/README.md">
                         GitHub Code and Architecture
                     </Link>
-                    <video width="640" height="480" controls autoPlay style={{border: "1px solid blue"}}>
+                    <video width="640" height="480" controls autoPlay muted style={{border: "1px solid blue"}}>
                         <source src="proof.mp4" type="video/mp4"/>
                     </video>
                 </Stack>
