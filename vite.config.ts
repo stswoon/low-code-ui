51f2c2c53d34a373830ba104b0ce6d381a32b33e
stswoon <xx@xx.xx>
2025-10-19 19:37:39 +0400

    deploy

--- vite.config.ts
+++ vite.config.ts
@@ -3,9 +3,9 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-    plugins: [
-        react(),
-    ],
-    build: {
+    plugins: [react()],
+    build: {},
+    preview: {
+        allowedHosts: ["low-code-ui.stswoon.ru"]
     }
 })
