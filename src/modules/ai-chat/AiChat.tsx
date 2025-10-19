a92142330d8ce6a9587264a98b4ae663966cd70f
stswoon <xx@xx.xx>
2025-10-19 15:34:59 +0400

    chat works but need history

--- src/modules/ai-chat/AiChat.tsx
+++ src/modules/ai-chat/AiChat.tsx
@@ -2,10 +2,12 @@ import {Box, Button, Input, Stack, TextareaAutosize} from '@mui/material';
 import {type FC, memo, useEffect, useState} from 'react';
 import {ChatOpenAI} from "@langchain/openai";
 import {isEven} from "../../shared/utils.ts";
+import {SYSTEM_AI_MSG, TEST_USER_MSG} from "../../shared/const.ts";
+import {marked} from "marked";
 
 export const AiChat: FC = memo(() => {
     const [chatGptKey, setChatGptKey] = useState('');
-    const [userPrompt, setUserPrompt] = useState('');
+    const [userPrompt, setUserPrompt] = useState(TEST_USER_MSG.trim());
     const [loading, setLoading] = useState(false);
     const [aiModel, setAiModel] = useState<ChatOpenAI | undefined>(undefined);
 
@@ -20,15 +22,25 @@ export const AiChat: FC = memo(() => {
 
     const sendAndHandleAiAnswer = async () => {
         let model = aiModel;
+        let firstSystemMsg;
         if (!model) {
             model = new ChatOpenAI({apiKey: chatGptKey, model: "gpt-4-1106-preview",});
             setAiModel(model);
+            firstSystemMsg = {role: "system", content: SYSTEM_AI_MSG.trim()};
         }
 
         // const response = await model.stream(new HumanMessage("Hello world!"));
         try {
             setLoading(true);
-            const response = await model.invoke(userPrompt);
+
+            //TODO: get type from model.invoke
+            const aiRequest = [];
+            if (firstSystemMsg) {
+                aiRequest.push(firstSystemMsg)
+            }
+            aiRequest.push({role: "user", content: userPrompt.trim()})
+
+            const response = await model.invoke(aiRequest);
             // console.log(response);
             //TODO: fix possible issue
             const newMsg = response.content as string;
@@ -55,22 +67,27 @@ export const AiChat: FC = memo(() => {
                        onChange={e => setChatGptKey(e.target.value)}/>
             </Stack>
             <Stack sx={{border: "1px solid blue", height: "250px", overflowY: 'scroll'}} gap={1}>
-                {messages.map(((msg, index) => (
-                    <Box key={msg} sx={{
-                        borderRadius: "8px",
-                        padding: "8px",
-                        marginLeft: isEven(index) ? "40px" : "0",
-                        marginRight: isEven(index) ? "0" : "40px",
-                        backgroundColor: isEven(index) ? "#d8f2de" : "#accdef"
-                    }}>{msg}</Box>
-                )))}
+                {messages.map(((msg, index) => {
+                    const html = marked.parse(msg);
+                    return <Box key={msg}
+                                sx={{
+                                    borderRadius: "8px",
+                                    padding: "8px",
+                                    marginLeft: isEven(index) ? "40px" : "0",
+                                    marginRight: isEven(index) ? "0" : "40px",
+                                    backgroundColor: isEven(index) ? "#d8f2de" : "#accdef"
+                                }}
+                                dangerouslySetInnerHTML={{__html: html}}
+                    />
+                    // }}>{msg}</Box>
+                }))}
             </Stack>
             <Stack gap={1} direction="row">
                 <TextareaAutosize style={{width: "500px"}} minRows={3} maxRows={3} placeholder="Type user prompt"
                                   value={userPrompt}
                                   onChange={e => setUserPrompt(e.target.value)}/>
                 <Button variant="contained" onClick={sendAndHandleAiAnswer}
-                        disabled={chatGptKey.trim().length === 0 || loading}
+                        disabled={chatGptKey.trim().length === 0 || loading || userPrompt.trim().length === 0}
                         loading={loading}>Send</Button>
                 <Button onClick={clear}>Clear</Button>
             </Stack>
--- src/shared/const.ts
+++ src/shared/const.ts
@@ -1,1 +1,51 @@
-export const API_SERVER_URL = "http://localhost:3000"
+export const API_SERVER_URL = "http://localhost:3000";
+
+export const TEST_USER_MSG = `
+Создай страницу с виджетом для получения списка пользователей. 
+Пользователей можно получить по url "/users". 
+У пользователя есть id, name, age, gender.
+`;
+
+//сдедующий вопрос убери ид из отображеия
+
+export const SYSTEM_AI_MSG = `
+- Ты эксперт по построение UI через json модель.
+- На основе пользовательского ввода тебе нужно создать JSON модель (Config), которая содержит массив страниц (Pages), 
+  в которые вкладываются виджеты (Widgets), в виджетах лежат поля (Fields). 
+  Также виджет имеет поле Datasource, через который можно настроить url для получения или отправки данных.
+- ВАЖНО! Результат ответа должен содержать ТОЛЬКО JSON модель.
+- Для Field.dataPath используй формат вида jsonPath, например для получение поля из респонса нужно записать "$.someField".
+- Описание моделей приведены ниже:
+
+export type Config = Page[];
+
+export interface Page {
+    id: string;
+    name: string;
+    urlPath: string;
+    widgets: Widget[];
+}
+
+export interface Widget {
+    id: string;
+    name: string;
+    type: 'Form' | 'CardList';
+    datasource: DataSource;
+    fields: Field[];
+}
+
+export interface Field {
+    id: string;
+    label: string;
+    type: 'text' | 'number' | 'hidden' | 'dropdown';
+    dataPath: string;
+    value?: unknown;
+    availableValues?: { id: string; value: string }[];
+}
+
+export interface DataSource {
+    type: 'fetch';
+    method: 'GET' | 'POST';
+    url: string;
+}
+`;
